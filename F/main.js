147c4fd7ebcee5efebca1cdfcea211db8c25a1b9
Nihal Priyadarshi <68635556+Nihal-Priyadarshi@users.noreply.github.com>
2021-09-22 14:28:16 +0530

    Create main.js

---
+++ F/main.js
@@ -0,0 +1,13 @@
+
+
+
+for(var i=0; i<5; i++) {
+    var d = document.createElement('div');
+    d.style.border = "2px solid blue";
+    d.style.height = "25px";
+    d.style.width = "50px";
+    var h = document.createElement('h1');
+    d.appendChild(h);
+    document.write("hii<br>");
+
+}
