32de0a7b18aaa31b0ce487abd2a4a3bdefcbef9d
Nihal Priyadarshi <68635556+Nihal-Priyadarshi@users.noreply.github.com>
2021-09-27 12:36:24 +0530

    Update main.js

--- F/main.js
+++ F/main.js
@@ -5,8 +5,8 @@ var main = document.querySelector('#main');
 for(var i=0; i<5; i++) {
     var d = document.createElement('div');
     d.style.border = "2px solid blue";
-    d.style.height = "25px";
-    d.style.width = "50px";
+    d.style.height = "100px";
+    d.style.width = "100px";
     var h = document.createElement('h1');
     h.innerHTML = "hii";
     d.appendChild(h);
